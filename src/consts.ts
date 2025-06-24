ac213993b220e6774af5a39c3be3303fb5abf48c
alcercu <333aleix333@gmail.com>
2025-06-24 13:40:13 +0200

    fix: move projects out of consts to prevent circular dependency

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,6 @@
 import { type Abi } from "viem";
 import { gnosis } from "viem/chains";
 
-import { sDaiAddress } from "@/generated";
-
 import { CowSwapAbi } from "@/abi/CowSwap";
 import { RouterAbi } from "@/abi/Router";
 import { sDAIAbi } from "@/abi/sDAI";
@@ -42,70 +40,6 @@ const contracts = {
 export const getContractInfo = (
   contractName: keyof typeof contracts,
 ): IContract => contracts[contractName];
-
-interface IProject {
-  name: string;
-  details: string;
-  color: string;
-  upToken: string;
-  downToken: string;
-  underlyingToken: `0x${number}`;
-  minValue: number;
-  maxValue: number;
-  precision: number;
-  marketId: string;
-}
-
-export const projects: Array<IProject> = [
-  {
-    name: "Project 1",
-    details: "aaaaaa",
-    color: "red",
-    upToken: "0x330797721e4F9773c02Ab6165292777E879E43f8",
-    downToken: "0x8932198b384F8cb090bBE825cF0a1061f436F053",
-    underlyingToken: sDaiAddress as `0x${number}`,
-    minValue: 0,
-    maxValue: 4,
-    precision: 10,
-    marketId: "0xa77dd0d6988f0f79b056d3196fa67f2488370909",
-  },
-  {
-    name: "Project 2",
-    details: "aaaaaa",
-    color: "blue",
-    upToken: "0x330797721e4F9773c02Ab6165292777E879E43f8",
-    downToken: "0x8932198b384F8cb090bBE825cF0a1061f436F053",
-    underlyingToken: sDaiAddress as `0x${number}`,
-    minValue: 0,
-    maxValue: 4,
-    precision: 10,
-    marketId: "0xa77dd0d6988f0f79b056d3196fa67f2488370909",
-  },
-  {
-    name: "Project 3",
-    details: "aaaaaa",
-    color: "green",
-    upToken: "0x330797721e4F9773c02Ab6165292777E879E43f8",
-    downToken: "0x8932198b384F8cb090bBE825cF0a1061f436F053",
-    underlyingToken: sDaiAddress as `0x${number}`,
-    minValue: 0,
-    maxValue: 4,
-    precision: 10,
-    marketId: "0xa77dd0d6988f0f79b056d3196fa67f2488370909",
-  },
-  {
-    name: "Project 4",
-    details: "aaaaaa",
-    color: "orange",
-    upToken: "0x330797721e4F9773c02Ab6165292777E879E43f8",
-    downToken: "0x8932198b384F8cb090bBE825cF0a1061f436F053",
-    underlyingToken: sDaiAddress as `0x${number}`,
-    minValue: 0,
-    maxValue: 4,
-    precision: 10,
-    marketId: "0xa77dd0d6988f0f79b056d3196fa67f2488370909",
-  },
-];
 
 export const cowSwapAppCode = "futarchy-test";
 
