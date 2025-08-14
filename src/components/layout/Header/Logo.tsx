1c1505c2684cc10a3c7be6add0bc8f529d7966b1
alcercu <333aleix333@gmail.com>
2025-08-14 12:11:46 +0200

    feat: update logos and icons

--- src/components/layout/Header/Logo.tsx
+++ src/components/layout/Header/Logo.tsx
@@ -1,17 +1,21 @@
 "use client";
 import React from "react";
 
+import { useTheme } from "next-themes";
+
 import Image from "next/image";
 import Link from "next/link";
 
 import _Logo from "@/assets/png/retro_PGF.png";
+import _LogoDark from "@/assets/png/retro_PGF_dark.png";
 
 const Logo: React.FC = () => {
+  const { theme } = useTheme();
   return (
     <div className="flex items-center gap-4">
       <Link href={"/"}>
         <Image
-          src={_Logo}
+          src={theme === "light" ? _Logo : _LogoDark}
           alt="RetroPGF experiment logo"
           className="size-14 max-h-14 hover:brightness-105 md:ml-6"
         />
