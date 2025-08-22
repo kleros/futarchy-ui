1035214cc963f331be4bf3fba4eeb8b21a117194
alcercu <333aleix333@gmail.com>
2025-08-22 12:21:24 +0200

    fix: make market-chart function static

--- src/hooks/useChartData.ts
+++ src/hooks/useChartData.ts
@@ -51,46 +51,38 @@ export const useChartData = (markets: Array<IMarket>) =>
   useQuery<IChartData[]>({
     queryKey: [`chart-${markets.map(({ marketId }) => marketId).join("-")}`],
     queryFn: async () => {
-      return await Promise.all(
-        markets.map(async (market) => {
-          const { marketId, maxValue, name, underlyingToken } = market;
-          const params = new URLSearchParams();
-          params.append("marketId", marketId);
-          params.append("chainId", "100");
-
-          const rawData: IReturn = await fetch(
-            `/api/market-chart?${params.toString()}`,
-          ).then((res) => res.json());
-
-          const processed: IChartData[""]["data"] = rawData[1].map(
-            (dataPoint) => {
-              let token0Price = dataPoint.token0Price;
-              let token1Price = dataPoint.token1Price;
-              const sqrtPrice = dataPoint.sqrtPrice;
-              if (
-                token0Price === "0" &&
-                token1Price === "0" &&
-                sqrtPrice &&
-                sqrtPrice !== "0"
-              ) {
-                ({ token0Price, token1Price } = getSqrtPrices(sqrtPrice));
-              }
-              return {
-                timestamp: dataPoint.periodStartUnix,
-                value:
-                  parseFloat(
-                    (dataPoint.pool.token0.id.toLowerCase() ===
-                    underlyingToken.toLowerCase()
-                      ? token0Price
-                      : token1Price
-                    ).slice(0, 9),
-                  ) * maxValue,
-              };
-            },
-          );
-
-          return { [name]: { market, data: processed } };
-        }),
-      );
+      const { data }: { data: IReturn[] } = await fetch(
+        "api/market-chart",
+      ).then((res) => res.json());
+      return data.map((rawData: IReturn, i: number) => {
+        const market = markets[i];
+        const processed: IChartData[""]["data"] = rawData[1].map(
+          (dataPoint) => {
+            let token0Price = dataPoint.token0Price;
+            let token1Price = dataPoint.token1Price;
+            const sqrtPrice = dataPoint.sqrtPrice;
+            if (
+              token0Price === "0" &&
+              token1Price === "0" &&
+              sqrtPrice &&
+              sqrtPrice !== "0"
+            ) {
+              ({ token0Price, token1Price } = getSqrtPrices(sqrtPrice));
+            }
+            return {
+              timestamp: dataPoint.periodStartUnix,
+              value:
+                parseFloat(
+                  (dataPoint.pool.token0.id.toLowerCase() ===
+                  market.underlyingToken.toLowerCase()
+                    ? token0Price
+                    : token1Price
+                  ).slice(0, 9),
+                ) * market.maxValue,
+            };
+          },
+        );
+        return { [market.name]: { market, data: processed } };
+      });
     },
   });
