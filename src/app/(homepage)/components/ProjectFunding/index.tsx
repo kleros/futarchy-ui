7094502305df1265e3c510f843025a90d87bcd25
Harman-singh-waraich <Waraichharman68@gmail.com>
2026-01-13 20:39:51 +0530

    feat: position-value-display-on-accordion

--- src/app/(homepage)/components/ProjectFunding/index.tsx
+++ src/app/(homepage)/components/ProjectFunding/index.tsx
@@ -7,6 +7,7 @@ import { useMarketsStore } from "@/store/markets";
 
 import { useMarketContext } from "@/context/MarketContext";
 import { useTradeWallet } from "@/context/TradeWalletContext";
+import { useTokenPositionValue } from "@/hooks/useTokenPositionValue";
 
 import CheckOutline from "@/assets/svg/check-outline-button.svg";
 import MinusOutline from "@/assets/svg/minus-outline.svg";
@@ -24,6 +25,23 @@ const ProjectFunding: React.FC = () => {
   });
   const { tradeExecutor } = useTradeWallet();
 
+  const { value: upValue } = useTokenPositionValue(
+    upToken,
+    underlyingToken,
+    tradeExecutor ?? "0x",
+    {
+      isUp: true,
+    },
+  );
+  const { value: downValue } = useTokenPositionValue(
+    downToken,
+    underlyingToken,
+    tradeExecutor ?? "0x",
+    {
+      isUp: false,
+    },
+  );
+  const totalValue = upValue + downValue;
   return (
     <CustomAccordion
       aria-label="card"
@@ -36,16 +54,29 @@ const ProjectFunding: React.FC = () => {
           title: (
             <>
               <div className="flex flex-1 flex-wrap items-center justify-between gap-4">
-                <div className="flex max-w-full grow basis-[70%] flex-col gap-8 md:min-w-[300px]">
+                <div className="flex max-w-full grow basis-[70%] flex-wrap gap-2 md:min-w-[300px]">
                   <div className="flex items-center gap-2">
                     <span
                       className="size-2 rounded-full"
                       style={{ backgroundColor: color }}
                     />
-                    <h3 className="text-klerosUIComponentsPrimaryText font-semibold">
+                    <h3 className="text-klerosUIComponentsPrimaryText text-left font-semibold">
                       {name}
                     </h3>
                   </div>
+                  {totalValue > 0 ? (
+                    <div className="flex items-center gap-2">
+                      <div className="border-klerosUIComponentsPrimaryText h-4 w-0 border-[0.5px] max-md:hidden" />
+
+                      <p className="text-klerosUIComponentsPrimaryText justify-center text-sm">
+                        Position:
+                        <span className="font-bold">
+                          {" "}
+                          {totalValue.toFixed(2)} sDAI{" "}
+                        </span>
+                      </p>
+                    </div>
+                  ) : null}
                 </div>
                 {isSelected ? (
                   <CheckOutline className="[&_path]:fill-klerosUIComponentsSuccess animate-fade-in size-4" />
