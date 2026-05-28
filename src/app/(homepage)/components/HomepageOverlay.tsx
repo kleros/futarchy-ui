a5f3fe2c0deb8a32c1c88036f42496891c9b6b95
Harman-singh-waraich <Waraichharman68@gmail.com>
2026-05-28 23:37:16 +0530

    chore: homepage-backdrop-scaling-fix

--- src/app/(homepage)/components/HomepageOverlay.tsx
+++ src/app/(homepage)/components/HomepageOverlay.tsx
@@ -6,7 +6,8 @@ const HomepageOverlay: React.FC = () => {
   return (
     <HomepageOverlaySvg
       className={clsx(
-        "absolute left-1/2 -z-1 max-w-360 -translate-x-1/2 translate-y-6",
+        "absolute left-1/2 -z-1 -translate-x-1/2 translate-y-6",
+        "w-[200%] origin-top scale-50 sm:w-[150%] sm:scale-75 md:w-full md:max-w-360 md:scale-95",
         "dark:[&_path]:fill-klerosUIComponentsWhiteBackground [&_path]:fill-[#4872FF05]",
       )}
     />
